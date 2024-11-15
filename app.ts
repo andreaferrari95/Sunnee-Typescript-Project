881ad542d3b452cb382d81ee8d146dcfe8ee8028
Andrea Ferrari | Phonetastik <131166715+andreaferrari95@users.noreply.github.com>
2024-11-15 11:42:00 +0100

    class Prodotto

--- app.ts
+++ app.ts
@@ -21,3 +21,36 @@ interface IProcessoProduzione {
   prodottiInProduzione: IProdotto[];
   aggiungiProdotto(prodotto: IProdotto): void;
 }
+
+class Prodotto implements IProdotto {
+  tipo: string;
+  id: string;
+  taglia: string;
+  colore: string;
+  stato: "disponibile" | "esaurito";
+
+  constructor(
+    tipo: string,
+    id: string,
+    taglia: string,
+    colore: string,
+    stato: "disponibile" | "esaurito"
+  ) {
+    this.tipo = tipo;
+    this.id = id;
+    this.taglia = taglia;
+    this.colore = colore;
+    this.stato = stato;
+  }
+
+  assegnaCliente(cliente: ICliente): void {
+    if (this.stato === "disponibile") {
+      console.log(
+        `Il cliente ${cliente.nome} ${cliente.cognome} ha ordinato il prodotto ${this.tipo} con id ${this.id} di taglia ${this.taglia} in colore ${this.colore}.`
+      );
+      this.stato = "esaurito";
+    } else {
+      console.log(`Prodotto ${this.tipo} non disponibile`);
+    }
+  }
+}
